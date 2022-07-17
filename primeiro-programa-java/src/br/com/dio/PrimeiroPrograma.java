82b5751c6b986088495df6ce6770f5ae286a1ddc
Luciano <lucianofelinto@gmail.com>
2022-07-17 10:49:32 -0300

    first commit

---
+++ primeiro-programa-java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		//System.out.println("Ola mundo, meu primeiro programa.");
+
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+	}
+
+}
+
+
