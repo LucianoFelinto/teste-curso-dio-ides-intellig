cc43c05da1678c26564bc34900adf15cc5e37a0e
Luciano <lucianofelinto@gmail.com>
2022-07-18 20:36:24 -0300

    First commit

---
+++ primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 3;
+        int soma = a + b;
+        System.out.println("Hello World " + soma);*/
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+}
